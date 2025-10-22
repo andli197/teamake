1f9571046ad1d68298b1d317370e1dea829e9f94
Andreas Lindblad <ALindblad@gmail.com>
2025-10-22 20:29:51 +0200

    Work in progress for saving values for other transients

--- lisp/teamake.el
+++ lisp/teamake.el
@@ -27,15 +27,39 @@
 
 (defun experimental-transient-scope-data-update-specific (scope prefix property new-value)
   (let* ((existing-scope (plist-get experimental-data scope 'string=))
-         (existing-values (plist-get existing-scope prefix)))
-    ;; existing values are a list dumbass!!!!
-    (plist-put existing-values property new-value)))
+         (existing-values (plist-get existing-scope prefix))
+         (suffix "="))
+    (if (s-ends-with? suffix property)
+        (setq suffix ""))
+    (plist-put existing-scope prefix
+               (seq-map
+                (lambda (opt)
+                  (if (s-starts-with? property opt)
+                      (concat property suffix new-value)
+                    opt))
+                existing-values)
+               )))
 
-(experimental-transient-scope-data-update-specific
- "c:/Program Files/Microsoft Visual Studio/2022/Community/"
- 'experimental
- "${project}="
- "hej knekt")
+(transient-define-suffix experimental-set-values ()
+  (interactive)
+
+  (experimental-transient-scope-data-update-specific
+   (transient-arg-value "--scope=" (transient-args 'experimental-child))
+   'experimental
+   (transient-arg-value "--parameter=" (transient-args 'experimental-child))
+   (transient-arg-value "--value=" (transient-args 'experimental-child))))
+
+(transient-define-prefix experimental-child ()
+  ["Experimental child"
+   ("s" "Set scope" "--scope="
+    :prompt "Scope: "
+    :reader transient-read-directory)
+   ("p" "Parameter" "--parameter="
+    :prompt "Parameter: ")
+   ("v" "Value" "--value="
+    :prompt "Value")
+   ("x" experimental-set-values
+    :description "Execute")])
 
 (defun experimental-transient-scope-data-update ()
   (interactive)
@@ -60,7 +84,10 @@
    ("p" "project" "${project}="
     :prompt "project: ")
    ("s" experimental-transient-scope-data-update
-    :description "Save values")]
+    :description "Save values"
+    :transient t)
+   ("c" experimental-child
+    :description "Child")]
   (interactive (list default-directory))
   (experimental-setup-from-scope scope)
   
